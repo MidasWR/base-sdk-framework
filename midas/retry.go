e0d0961f9dd8e700ce41d1a9f577cf4f81f90eaf
midas <danila6633516@gmail.com>
2025-07-18 18:11:35 +0300

    Retry logic

---
+++ midas/retry.go
@@ -0,0 +1,29 @@
+package midas
+
+import (
+	"github.com/rs/zerolog"
+	"time"
+)
+
+type RetryConfig[T any] struct {
+	Config   T
+	Logger   *zerolog.Logger
+	Attempts int
+}
+
+func RetryFunc[T any](log zerolog.Logger, cfg RetryConfig[T], start func(config T, log zerolog.Logger) error) {
+	for i := 0; i < cfg.Attempts; i++ {
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Error().Msgf("panic caught: %v", r)
+				}
+			}()
+			time.Sleep(time.Duration(1<<i) * time.Second)
+			log.Info().Msgf("%d try of start", i)
+			if err := start(cfg.Config, log); err != nil {
+				log.Error().Msgf("error starting %d try: %v", i, err)
+			}
+		}()
+	}
+}
